cfbc2a2ba583f913d028ca84b2c8bc981762abe2
Nessrine88 <macherki.nessrine@yahoo.com>
2025-04-21 19:04:12 +0100

    add the header

---
+++ src/app/components/Header/Logo.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import logo from '@/public/profile-img.png'
+import Image from 'next/image'
+
+const Logo = () => {
+  return (
+    <Link href="/" className='flex items-center text-dark'>
+      <div className='w-16 rounded-full overflow-hidden border border-solid border-dark mr-4'>
+    <Image src={logo} alt="Codebucks"className='w-full h-auto rounded-full' />
+    </div>
+    <span>CodeBucks</span>
+    </Link>
+  )
+}   
+
+export default Logo
